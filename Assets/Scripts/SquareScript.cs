0f7e89253772253cccf9a085ca6e5695b078b79b
Vladyslav Karlinskyi <91985923+karlvava16@users.noreply.github.com>
2024-10-31 11:34:28 +0200

    Initial commit

---
+++ Assets/Scripts/SquareScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SquareScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world! ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
