22d2e62e60ec70874fb2f56b44bd1358a75002af
Vladyslav Karlinskyi <91985923+karlvava16@users.noreply.github.com>
2024-11-06 10:20:25 +0200

    06.11
    06.11

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -6,6 +6,10 @@ public class BirdScript : MonoBehaviour
     [SerializeField]
     private Transform arrow;
     private Rigidbody2D rb2d;
+    [SerializeField]
+    private float minForce = 500f;
+    [SerializeField]
+    private float maxForce = 2000f;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -13,8 +17,10 @@ public class BirdScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input. GetKeyDown(KeyCode.Space))
         {
+            float forceAmplitude = minForce +
+                                   (maxForce - minForce) * ForceIndicatorScript. forceFactor;
             rb2d.AddForce(arrow.right * 1000f);
         }
     }
